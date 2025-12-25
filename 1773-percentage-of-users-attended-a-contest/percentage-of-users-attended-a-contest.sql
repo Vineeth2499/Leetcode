c739428edf881bdfcac94657873cb24ac2f3f045
Vineeth Arumugam <141380226+Vineeth2499@users.noreply.github.com>
2025-12-26 01:37:04 +0530

    Time: 910 ms (74.14%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,5 @@
+SELECT contest_id,
+       ROUND(COUNT(user_id)*100.00/(SELECT COUNT(user_id) FROM Users), 2) AS percentage
+FROM Register
+GROUP BY contest_id
+ORDER BY Percentage DESC, contest_id;
