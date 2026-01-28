877709b7cfc6760695837da2ab3d29f8694cee68
Vineeth Arumugam <141380226+Vineeth2499@users.noreply.github.com>
2026-01-28 21:47:00 +0530

    Create First-Attempt(Failed on Edge Case).sql

---
+++ 1882-the-number-of-employees-which-report-to-each-employee/First-Attempt(Failed on Edge Case).sql
@@ -0,0 +1,15 @@
+SELECT
+  m.employee_id,
+  m.name,
+  COUNT(e.name) AS reports_count,
+  CEIL(AVG(e.age)) AS average_age
+FROM
+  Employees e
+JOIN
+  Employees m
+ON
+  e.reports_to = m.employee_id
+GROUP BY
+  m.employee_id, m.name
+ORDER BY
+  m.employee_id;
