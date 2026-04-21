379392be1d35abd85102c6288d2f8d1c3e256f77
Vineeth Arumugam <141380226+Vineeth2499@users.noreply.github.com>
2026-04-21 23:13:12 +0530

    Remove SQL query for finding COVID recovery patients

--- 3586-Find-COVID-Recovery-Patients/3586-Find-COVID-Recovery-Patients.sql
+++ 3586-Find-COVID-Recovery-Patients/3586-Find-COVID-Recovery-Patients.sql
@@ -1,18 +1,1 @@
-SELECT
-   p.patient_id,
-   patient_name,
-   age,
-   MAX(CASE WHEN result = 'Negative' THEN test_date END) -
-   MAX(CASE WHEN result = 'Positive' THEN test_date END) AS recovery_time
-FROM
-   patients p
-LEFT JOIN
-   covid_tests c ON p.patient_id = c.patient_id
-GROUP BY
-   1,
-   2,
-   3
-HAVING
-   recovery_time IS NOT NULL
-ORDER BY
-   recovery_time ASC, 1 ASC
+
