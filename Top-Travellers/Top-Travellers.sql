a198a527c462118225aca6c6a8c95d52f38f5626
Vineeth Arumugam <141380226+Vineeth2499@users.noreply.github.com>
2026-05-09 22:46:13 +0530

    Create Top-Travellers.sql

---
+++ Top-Travellers/Top-Travellers.sql
@@ -0,0 +1,13 @@
+SELECT
+  name,
+  COALESCE(SUM(distance), 0) AS travelled_distance
+FROM
+  users u
+LEFT JOIN
+  rides r
+ON u.id = r.user_id
+GROUP BY
+  name
+ORDER BY
+  travelled_distance DESC,
+  name ASC
