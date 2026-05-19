abf036f3ae0ae65e7b13e81413bff57361e0b377
Vineeth Arumugam <141380226+Vineeth2499@users.noreply.github.com>
2026-05-19 22:52:34 +0530

    Add SQL query for first letter capitalization

--- 3374-First-Letter-Capitalization-II/3374-First-Letter-Capitalization-II.sql
+++ 3374-First-Letter-Capitalization-II/3374-First-Letter-Capitalization-II.sql
@@ -1,1 +1,12 @@
-
+SELECT
+  content_id,
+  content_text AS original_text,
+  ARRAY_TO_STRING(ARRAY(
+    SELECT ARRAY_TO_STRING(ARRAY(
+        SELECT UPPER(LEFT(LOWER(part), 1)) || SUBSTRING(LOWER(part), 2)
+        FROM UNNEST(STRING_TO_ARRAY(LOWER(word), '-')) AS part
+    ), '-')
+    FROM UNNEST(STRING_TO_ARRAY(content_text, ' ')) AS word
+  ), ' ') AS converted_text
+FROM
+  user_content;
