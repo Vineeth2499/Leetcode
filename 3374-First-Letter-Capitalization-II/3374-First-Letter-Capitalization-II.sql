79e4841d6366da946b0a1b79873bcf4b091aaab6
Vineeth Arumugam <141380226+Vineeth2499@users.noreply.github.com>
2026-05-21 21:18:11 +0530

    Update 3374-First-Letter-Capitalization-II.sql

--- 3374-First-Letter-Capitalization-II/3374-First-Letter-Capitalization-II.sql
+++ 3374-First-Letter-Capitalization-II/3374-First-Letter-Capitalization-II.sql
@@ -1,16 +1,1 @@
-SELECT
-  content_id,
-  content_text AS original_text,
-  ARRAY_TO_STRING(ARRAY(
-    SELECT ARRAY_TO_STRING(ARRAY(
-     SELECT
-      CASE
-        WHEN part ~ '^[a-zA-Z]' THEN UPPER(LEFT(LOWER(part), 1)) || SUBSTRING(LOWER(part), 2)
-        ELSE LOWER(part)
-      END
-    FROM UNNEST(STRING_TO_ARRAY(word, '-')) AS part
-  ), '-')
-  FROM UNNEST(STRING_TO_ARRAY(content_text, ' ')) AS word
-  ), ' ') AS converted_text
-FROM
-  user_content
+
